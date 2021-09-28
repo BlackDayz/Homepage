5acc8cd3ba6058155accb89c4bd78a9f1b2d42ef
Mittelblut9 <sauer.benedikt@outlook.de>
2021-09-28 22:04:24 +0200

    added homepage header

--- src/components/homepage/hp-header.vue
+++ src/components/homepage/hp-header.vue
@@ -1,10 +1,18 @@
 <template>
-  <div class="header">
-    <video autoplay muted loop>
-      <source
-        src="https://www.blackdayz.de/src/mp4/Website_Video_zusammenfassung.mp4"
-        type="video/mp4"
-      />
-    </video>
+  <div class="hp_header">
+    <img src="../../assets/img/logo/BlackDayzLogo.png" alt="" class="hp_header_logo absolute left left-0"/>
+    <div class="hp_header_txt white absolute">
+      <h1>Frontend</h1>
+      <h1>Backend</h1>
+    </div>
+    <div class="hp_header_nav absolute white textright">
+      <p><router-link to="/">About</router-link></p>
+      <p><router-link to="/">Referenzen</router-link></p>
+    </div>
+    <img src="../../assets/img/header/hp_header_img_bg.jpg" alt="" class="hp_header_img_bg absolute object-fit-cover" />
   </div>
 </template>
+
+<style scoped>
+@import url('../../assets/css/hp-header/hp-header.css');
+</style>
