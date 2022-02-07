5de8908cf6ae79b444f268c05a5ccb51d4ca9ac8
Mittelblut9 <sauer.benedikt@outlook.de>
2022-02-07 20:20:40 +0100

    added cookie interaction to "click for add a note"

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,12 @@
+const dc_note_cookie = 'dc_note'
+
+window.addEventListener('load', function () {
+    if(getCookie(dc_note_cookie)) {
+        document.getElementById('dc_profile_note').innerHTML = getCookie(dc_note_cookie)
+    }
+});
+
+
 var x = document.querySelectorAll('.refercence_ct');
 
 Array.from(x).forEach(b => {
@@ -12,3 +21,30 @@ document.getElementById('copy_dcname').addEventListener('click', () => {
         alert('Discord username copied');
     }).catch(err => {return;})
 });
+
+document.getElementById('dc_profile_note').addEventListener('focusout', function() {
+    return setCookie(dc_note_cookie, this.value, 356);
+});
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/;samesite=strict;secure";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return false;
+  }
