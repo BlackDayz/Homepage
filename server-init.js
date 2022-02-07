e4da106a8b58c7bd1068b0bbff54e0b116002aec
Mittelblut9 <sauer.benedikt@outlook.de>
2022-02-07 20:34:08 +0100

    Update server-init.js

--- server-init.js
+++ server-init.js
@@ -21,7 +21,7 @@ module.exports = (app) => {
         res.header("Access-Control-Allow-Methods", nconf.get('cors:headers:Access-Control-Allow-Methods'));
         res.header("Access-Control-Allow-Headers", nconf.get("cors:headers:Access-Control-Allow-Headers"));
         res.header("Access-Control-Max-Age", nconf.get('cors:headers:Access-Control-Max-Age'));
-        res.header("default-src 'self' https:\/\/*.blackdayz.de/ ")
+        res.header("Content-Security-Policy", "default-src 'self' https:\/\/blackdayz.de/")
         res.header("withCredentials", nconf.get('cors:headers:withCredentials'));
         res.header("Content-Type", nconf.get('cors:headers:Content-Type'));
         next();
