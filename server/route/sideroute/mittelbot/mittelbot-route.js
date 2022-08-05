7036f8a788284a3f60ab26e39359dba4d0d10a62
Mittelblut9 <sauer.benedikt@outlook.de>
2022-08-05 11:35:10 +0200

    fixed routing to /mittelbot

--- server/route/sideroute/mittelbot/mittelbot-route.js
+++ server/route/sideroute/mittelbot/mittelbot-route.js
@@ -1,7 +1,7 @@
 const nconf = require('nconf');
 
 module.exports = ({app}) => {
-    app.get('/'+nconf.get('routing:mittelbot:homepag'), async (req, res) => {
+    app.get('/'+nconf.get('routing:mittelbot:homepage'), async (req, res) => {
         res.redirect('https://mittelbot.blackdayz.de');
     });
 }
