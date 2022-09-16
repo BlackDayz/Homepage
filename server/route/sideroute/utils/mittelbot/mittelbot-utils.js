715039521845870ccf52e8a9266cea6ef8556713
Mittelblut9 <sauer.benedikt@outlook.de>
2022-09-16 20:29:56 +0200

    Added: Mittelbot logo

---
+++ server/route/sideroute/utils/mittelbot/mittelbot-utils.js
@@ -0,0 +1,20 @@
+const nconf = require('nconf');
+const fs = require('fs')
+const path = require('path');
+
+module.exports = ({app}) => {
+    app.get('/'+nconf.get('utils:mittelbot_logo:path'), (req, res) => {
+        const filePath = path.join(__dirname, 'mittelbot_logo.png');
+        const stat = fs.statSync(filePath);
+
+        res.writeHead(200, {
+            'Content-Type': 'image/png',
+            'Content-Length': stat.size,
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET",
+        });
+
+        const readStream = fs.createReadStream(filePath);
+        return readStream.pipe(res);
+    });
+}
