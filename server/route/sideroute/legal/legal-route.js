d5ad77bf6f70e7f06907f233d6156e055fc65c63
Mittelblut9 <sauer.benedikt@outlook.de>
2022-10-28 13:15:06 +0200

    Fixed missing route

--- server/route/sideroute/legal/legal-route.js
+++ server/route/sideroute/legal/legal-route.js
@@ -1,11 +1,11 @@
 const nconf = require('nconf');
 
 module.exports = ({app}) => {
-    app.get('/'+nconf.get('routing:impressum:path'), async (req, res) => {
-        res.render('legal/' + nconf.get('routing:impressum'))
+    app.get(nconf.get('routing:impressum:path'), async (req, res) => {
+        res.render('legal/impressum')
     });
 
-    app.get('/'+nconf.get('routing:privacy'), async (req, res) => {
-        res.render('legal/' + nconf.get('routing:privacy'))
+    app.get(nconf.get('routing:privacy:path'), async (req, res) => {
+        res.render('legal/privacy.ejs')
     });
 }
