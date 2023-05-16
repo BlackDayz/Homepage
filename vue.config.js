6e0b88ea41ced5d060f9d4f701d476621f40e058
Mittelblut9 <info@blackdayz.de>
2023-05-16 17:37:37 +0200

    fix/gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'Production' ? '/BlackDayz-Homepage/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/BlackDayz-Homepage/' : '/'
 }
