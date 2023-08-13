bbf021525e4e55a47600e74460ba7417894cce61
Mittelblut9 <info@blackdayz.de>
2023-08-13 21:07:01 +0200

    feat: add nav header

--- src/views/App.vue
+++ src/views/App.vue
@@ -1,4 +1,5 @@
 <template>
+    <BlackDayzHeader />
     <router-view />
     <BlackDayzFooter />
 </template>
@@ -10,12 +11,14 @@
 </style>
 
 <script>
+import BlackDayzHeader from '@/components/_header/header.vue';
 import BlackDayzFooter from '@/components/_footer/footer.vue';
 
 export default {
     name: 'App',
     components: {
         BlackDayzFooter,
+        BlackDayzHeader,
     },
 };
 </script>
