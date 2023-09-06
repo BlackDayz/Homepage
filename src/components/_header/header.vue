bb0769b62ddbe7ccf19fec3dc739854f927196b0
Mittelblut9 <sauer.benedikt@outlook.de>
2023-09-06 11:36:00 +0200

    feat: redesign top page (#85)

--- src/components/_header/header.vue
+++ src/components/_header/header.vue
@@ -9,6 +9,24 @@
 <script>
 export default {
     name: 'BlackDayzHeader',
+
+    mounted() {
+        document.addEventListener('scroll', this.handleScroll);
+    },
+
+    methods: {
+        handleScroll() {
+            const nav = document.querySelector('.nav');
+            const topPage = document.querySelector('.top_page');
+            const scroll = window.scrollY;
+
+            if (scroll > 700) {
+                nav.style.visibility = 'visible';
+            } else {
+                nav.style.visibility = 'hidden';
+            }
+        },
+    },
 };
 </script>
 
