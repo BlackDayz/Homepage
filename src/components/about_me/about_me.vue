055a63aa93df92de4000688bc9af1ac9af7ef388
Mittelblut9 <sauer.benedikt@outlook.de>
2023-09-06 11:55:22 +0200

    feat: redesign about me section

--- src/components/about_me/about_me.vue
+++ src/components/about_me/about_me.vue
@@ -1,26 +1,36 @@
 <template>
     <div class="about_me_left white left">
-        <p>
-            <img :src="require('@/assets/img/icons/hello-48.png')" width="42" /> Hey, my name is
-            obviously Mittelblut9 I'm 20 years old and a <span class="red">Junior</span> Web
-            Developer living in Germany
-        </p>
+        <div>
+            <img :src="require('@/assets/img/icons/hello-48.png')" width="42" />
+            <p>
+                Hey, my name is obviously Mittelblut9 I'm 20 years old and a
+                <span class="red">Junior</span> Web Developer living in Germany
+            </p>
+        </div>
         <br />
-        <p>
-            <img :src="require('@/assets/img/icons/icons8-prize-48.png')" width="42" />Official
-            trained
-            <strong>technical design assistant</strong>
-        </p>
+        <div>
+            <img :src="require('@/assets/img/icons/icons8-prize-48.png')" width="42" />
+            <p>
+                Official trained
+                <strong>technical design assistant</strong>
+            </p>
+        </div>
         <br />
-        <p>
-            <img :src="require('@/assets/img/icons/degree-42.png')" width="42" />At the moment I am
-            completing my training as a <strong>IT specialist for application development</strong>
-        </p>
+        <div>
+            <img :src="require('@/assets/img/icons/degree-42.png')" width="42" />
+            <p>
+                At the moment I am completing my training as a
+                <strong>IT specialist for application development</strong>
+            </p>
+        </div>
         <br />
-        <p>
-            <img :src="require('@/assets/img/icons/cloud-48.png')" width="42" />Most of the days i'm
-            working on projects like websites or my own Discord Bot.. or uhm.. just playing games :)
-        </p>
+        <div>
+            <img :src="require('@/assets/img/icons/cloud-48.png')" width="42" />
+            <p>
+                Most of the days i'm working on projects like websites or my own Discord Bot.. or
+                uhm.. just playing games :)
+            </p>
+        </div>
     </div>
 </template>
 
@@ -30,3 +40,5 @@ export default {
     name: 'AboutMe',
 };
 </script>
+
+<style src="@/assets/css/about_me/aboutme.css" />
